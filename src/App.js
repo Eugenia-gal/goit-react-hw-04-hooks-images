9d5af06e89445ab58b7ae1e84993310d29ffa5ee
Eugenia-gal <galevbor12@gmail.com>
2021-08-02 17:40:08 +0300

    convert App class to function with hooks

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { SearchBar } from './Components/SearchBar/SearchBar';
+import ImageGallery from 'Components/ImageGallery/ImageGallery';
+import Modal from 'Components/Modal/Modal';
+import './App.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
+export function App() {
+  const [searchQuery, setsearchQuery] = useState('');
+  const [selectedImage, setselectedImage] = useState(null);
+  const [selectedImageTags, setselectedImageTags] = useState(null);
+
+  return (
+    <div className="App">
+      <SearchBar
+        onSubmit={text => {
+          setsearchQuery(text);
+        }}
+      />
+      <ImageGallery
+        query={searchQuery}
+        onSelect={({ largeImage, tags }) => {
+          setselectedImage(largeImage);
+          setselectedImageTags(tags);
+        }}
+      />
+      {selectedImage && (
+        <Modal
+          src={selectedImage}
+          tags={selectedImageTags}
+          onClose={() => {
+            setselectedImage(null);
+          }}
+        />
+      )}
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
